dc063915b961f60d5691fc699634b92742fdd77f
Ricky Reusser <rsreusser@gmail.com>
2015-05-11 05:10:32 -0400

    Tests for div, diveq

--- test/test.js
+++ test/test.js
@@ -6,7 +6,9 @@ var ops = require("ndarray-ops")
 var zeros = require("zeros")
 var unpack = require("ndarray-unpack")
 
-require("tape").test("ndarray-complex", function(t) {
+var test = require("tape");
+
+test("ndarray-complex", function(t) {
 
   var a_r = zeros([3])
     , a_i = zeros([3])
@@ -36,3 +38,58 @@ require("tape").test("ndarray-complex", function(t) {
 
   t.end()
 })
+
+test("diveq",function(t) {
+  var a_r = ndarray([1,1]),
+      a_i = ndarray([2,2]),
+      b_r = ndarray([3,3]),
+      b_i = ndarray([4,4]),
+      c_r = zeros([3]),
+      c_i = zeros([3]);
+
+  cops.diveq(a_r, a_i, b_r, b_i);
+
+  t.assert( Math.abs(a_r.get(0) - 0.44 ) < 1e-8 );
+  t.assert( Math.abs(a_i.get(0) - 0.08 ) < 1e-8 );
+  t.assert( Math.abs(a_r.get(1) - 0.44 ) < 1e-8 );
+  t.assert( Math.abs(a_i.get(1) - 0.08 ) < 1e-8 );
+
+  t.end();
+
+});
+
+test("diveq",function(t) {
+  var a_r = ndarray([1,1]),
+      a_i = ndarray([2,2]),
+      b_r = ndarray([3,3]),
+      b_i = ndarray([4,4]);
+
+  cops.diveq(a_r, a_i, b_r, b_i);
+
+  t.assert( Math.abs(a_r.get(0) - 0.44 ) < 1e-8 );
+  t.assert( Math.abs(a_i.get(0) - 0.08 ) < 1e-8 );
+  t.assert( Math.abs(a_r.get(1) - 0.44 ) < 1e-8 );
+  t.assert( Math.abs(a_i.get(1) - 0.08 ) < 1e-8 );
+
+  t.end();
+
+});
+
+test("div",function(t) {
+  var a_r = ndarray([1,1]),
+      a_i = ndarray([2,2]),
+      b_r = ndarray([3,3]),
+      b_i = ndarray([4,4]),
+      c_r = zeros([3]),
+      c_i = zeros([3]);
+
+  cops.div(c_r, c_i, a_r, a_i, b_r, b_i);
+
+  t.assert( Math.abs(c_r.get(0) - 0.44 ) < 1e-8 );
+  t.assert( Math.abs(c_i.get(0) - 0.08 ) < 1e-8 );
+  t.assert( Math.abs(c_r.get(1) - 0.44 ) < 1e-8 );
+  t.assert( Math.abs(c_i.get(1) - 0.08 ) < 1e-8 );
+
+  t.end();
+
+});
